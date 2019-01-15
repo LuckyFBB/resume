4893c15db04be258fb133b054a5f8dfe8af0c19c
LuckyFBB <976060700@qq.com>
2019-01-15 14:07:07 +0800

    update about

--- src/components/container/mb/mb_about/about.js
+++ src/components/container/mb/mb_about/about.js
@@ -10,7 +10,7 @@ export default function MbAbout() {
           <img src={mine} />
         </div>
         <div className="mabout__intro">
-          <div>我叫LuckyFBB，目前生活在浙江杭州，目前大四，浙江工业大学软件工程学生。是一名有灵魂的程序媛。希望能够成为一名优秀的前端工程师。</div>
+          <div>我叫LuckyFBB，大四在读，浙江工业大学软件工程学生，希望能够成为一名优秀的前端工程师。</div>
           <div>平时除了学习之外，喜欢出去拍照，爬山，也会和朋友在峡谷相约，或者再吃把鸡。</div>
           <div>有自己短期的目标，能够高效率的完成所设定的目标，也坚定要做一个执行力很强的人。</div>
         </div>
